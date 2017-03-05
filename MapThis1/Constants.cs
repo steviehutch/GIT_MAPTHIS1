c2b53ae05b82508fee822f66dddd814c8e32542e
hutch <steviehutch@gmail.com>
2017-03-05 19:55:42 +0000

    Add project files.

---
+++ MapThis1/Constants.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace MapThis1
+{
+	public static class Constants
+	{
+		// Replace strings with your Azure Mobile App endpoint.
+		public static string ApplicationURL = @"https://mapthis1.azurewebsites.net";
+	}
+}
+
